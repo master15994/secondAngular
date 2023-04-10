f40fc395c7eb18dfc1ad8c22e4ca5d15141f550b
Askar Pulatov <master15994@gmail.com>
2023-04-10 10:45:08 +0300

    finish pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ErrorHandler } from '@angular/core';
+import { withNavigationErrorHandler } from '@angular/router';
+import { timeout } from 'rxjs';
+
+export interface Post {
+  title: string;
+  text: string;
+  [key: string]: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'secondAngular';
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular Newest',
+      text: 'Bekan new Suit',
+    });
+  }
+  search = '';
+  searchField = 'title';
+  posts: Post[] = [
+    { title: 'Beer', text: 'Самое лучшие пиво в мире' },
+    { title: 'Bread', text: 'Самое лучшие хлеб в мире' },
+    { title: 'JS', text: 'Самое лучшие язык в мире' },
+  ];
+  e: number = Math.E;
+  str = 'see u tomorrow ';
+  date: Date = new Date();
+  float = 0.42;
+
+  obj = {
+    a: 1,
+    b: 1,
+    c: { e: 2, f: 3 },
+    g: {
+      k: {
+        l: 1,
+      },
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { PipesComponent } from './pipes/pipes.component';
+
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
